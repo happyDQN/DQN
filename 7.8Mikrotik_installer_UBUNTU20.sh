b307fb83da7f0d7bb212e07dff9bbc3d69fb0e08
happyDQN <128605237+happyDQN@users.noreply.github.com>
2023-04-01 00:37:54 +0800

    Update and rename 7.7Mikrotik_installer.sh to 7.8Mikrotik_installer_UBUNTU20.sh

--- 7.8Mikrotik_installer_UBUNTU20.sh
+++ 7.8Mikrotik_installer_UBUNTU20.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "Welcome to Cloudzy! MikroticOS will be installed on your VPS in a few seconds :)"
-wget https://download.mikrotik.com/routeros/7.7/chr-7.7.img.zip -O chr.img.zip  && \
+echo "Welcome to Cloudzy! MikroticOS will be installed on your Ubuntu20.00 LTS VPS in a few seconds :)"
+wget https://download.mikrotik.com/routeros/7.8/chr-7.8.img.zip -O chr.img.zip  && \
 gunzip -c chr.img.zip > chr.img  && \
 mount -o loop,offset=33571840 chr.img /mnt && \
 ADDRESS=`ip addr show eth0 | grep global | cut -d' ' -f 6 | head -n 1` && \
