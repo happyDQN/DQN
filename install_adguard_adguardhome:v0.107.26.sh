573794fbbbdf9fdae5bc493749e4e989c17b69b5
happyDQN <128605237+happyDQN@users.noreply.github.com>
2023-03-31 23:56:11 +0800

    Create install_adguard_adguardhome:v0.107.26.sh

---
+++ install_adguard_adguardhome:v0.107.26.sh
@@ -0,0 +1,7 @@
+# docker install scripts
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+#  docker pull adguard/adguardhome:v0.107.26
+#  docker save adguard/adguardhome:v0.107.26 > adguard.tar
+
