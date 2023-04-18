9c61fd426535b9a271fdee06d52161ff03ff0e71
happyDQN <128605237+happyDQN@users.noreply.github.com>
2023-04-18 17:02:26 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -5,3 +5,17 @@ docker start id # 运行docker <id>
 docker ps    # 查看正在运行的docker 
 
 docker rename 老名字 新名字   # 重命名id
+
+
+
+wget -qO- https://get.docker.com/ | bash  # 安装docker
+
+docker pull dnomd343/cleardns   # 拉取docker hub镜像
+
+docker run -dt --network host \
+  --restart always \
+  --name cleardns --hostname cleardns \
+  --volume /etc/cleardns/:/cleardns/ \
+  --volume /etc/timezone:/etc/timezone:ro \
+  --volume /etc/localtime:/etc/localtime:ro \
+  dnomd343/cleardns
