bf6d8bb5871da73f32c2eb63a1bc3dbde08bff07
happyDQN <128605237+happyDQN@users.noreply.github.com>
2023-04-18 18:25:04 +0800

    Update and rename docker.sh to docker_cleardns.sh

--- docker_cleardns.sh
+++ docker_cleardns.sh
@@ -12,10 +12,6 @@ wget -qO- https://get.docker.com/ | bash  # 安装docker
 
 docker pull dnomd343/cleardns   # 拉取docker hub镜像
 
-docker run -dt --network host \
-  --restart always \
-  --name cleardns --hostname cleardns \
-  --volume /etc/cleardns/:/cleardns/ \
-  --volume /etc/timezone:/etc/timezone:ro \
-  --volume /etc/localtime:/etc/localtime:ro \
-  dnomd343/cleardns
+docker save dnomd343/cleardns > cleardns.tar
+
+
