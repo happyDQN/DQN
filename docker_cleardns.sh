f2effebe0c4bff156c6155b28f21cfa7c38b9e30
happyDQN <128605237+happyDQN@users.noreply.github.com>
2023-04-18 18:32:07 +0800

    Update docker_cleardns.sh

--- docker_cleardns.sh
+++ docker_cleardns.sh
@@ -15,3 +15,22 @@ docker pull dnomd343/cleardns   # 拉取docker hub镜像
 docker save dnomd343/cleardns > cleardns.tar
 
 
+
+# /system/device-mode/update container=yes      # reset or reboot ENABLE docker
+
+
+# /container/config/set registry-url=https://registry-1.docker.io tmpdir=/pull 
+
+
+create VETH_dns
+
+Address=172.17.0.3/24 Gateway=172.17.0.1
+
+create bridge
+
+add address for bridge      Address= 172.17.0.1/24
+add bridge port for VETH
+
+IP - DNS - enable Allow Remote Requests
+
+DNS=172.17.0.3
